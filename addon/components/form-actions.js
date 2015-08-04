7d58381386c7695e19a2479415a74460859b0d50
Sean <seangores@gmail.com>
2015-08-03 20:02:28 -0600

    Initial gh-pages commit

--- addon/components/form-actions.js
+++
@@ -1,9 +0,0 @@
-import Ember from "ember";
-import layout from "../templates/components/form-actions";
-
-export default Ember.Component.extend({
-	layout: layout,
-	tagName: "footer",
-	classNameBindings: ["defaultClassName"],
-	defaultClassName: "form-actions"
-});
